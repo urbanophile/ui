BUG Fixed waveform array bug which was because of sign error

pvapp/hardware/daq.py
@@ -324,7 +324,7 @@ class LightPulse():
         assert metadata.sample_rate > 0
 
         self.Waveform = metadata.waveform
-        self.A = - metadata.amplitude
+        self.A = metadata.amplitude
         self.Offset_Before = metadata.offset_before
         self.Offset_After = metadata.offset_after
         self.Duration = metadata.duration
@@ -357,6 +357,7 @@ class LightPulse():
         Factory method to produce numpy array with light intensity values
         """
         # pad waveform with zeroes
+
         v_before = np.zeros(
             int(self.output_samples * self.Offset_Before / 1000)
         )
@@ -372,8 +373,14 @@ class LightPulse():
 
         else:
             self.time_array = np.linspace(
-                0, self.Duration, num=self.output_samples * self.Duration
+                0, self.Duration, num=(self.output_samples * self.Duration)
             )
+            print "Duration", self.Duration
+            print "self.time_array", self.time_array.shape
+            print "time_array", self.time_array
+            print "A: ", self.A
+
+
             # TODO: this is bad, refactor
             voltage_waveform = getattr(self, self.Waveform)(
                 self.time_array, self.A
@@ -381,6 +388,7 @@ class LightPulse():
             voltage_waveform = self.scale_to_threshold(voltage_waveform)
 
         self.Duration = self.time_array[-1]
+        # print (v_before, v_after)
         complete_waveform = np.concatenate(
             (v_before, voltage_waveform, v_after)
         )

pvapp/test/test_experimentsettings.py
@@ -7,36 +7,39 @@ class ExperimentSettingsTest(unittest.TestCase):
     # np.set_printoptions(threshold='nan')
     def setUp(self):
         self.meta_data = ExperimentSettings()
+        self.maxDiff = None
 
-    def get_settings_as_dict(self):
+    def test_get_settings_as_dict(self):
         meta_data = {
-            'averaging': 1,
-            'Measurement_Binning': 1,
-            'Threshold_mA': 150,
-            'sample_rate': 1.2e3,
             'channel': 'High (2A/V)',
-            'threshold': 150.0,
-
+            'averaging': 1,
+            'measurement_binning': 1,
+            'threshold_mA': 150,
             "inverted_channels": {
                 'Reference': False,
                 'PC': False,
                 'PL': True
             },
 
+            'sample_rate': 1.2e3,
+            'output_sample_rate': 1.2e3,
+
             "InputVoltageRange": 10.0,
-            "OutputVoltageRange": 5,
+            "OutputVoltageRange": 10.0,
             "waveform": 'Sin',
             "amplitude": 0.5,
+
             "offset_before": 1,
             "offset_after": 10,
             "duration": 1,
-            "voltage_threshold": 150.0,
-            "channel_name": None,
+            "voltage_threshold": 0.08152173913043478,
+            "channel_name": 'ao0',
 
             #
-            "pc_calibration_mean": 0,
-            "pc_calibration_std": 0
+            "pc_calibration_mean": None,
+            "pc_calibration_std": None
         }
+        self.assertDictEqual(self.meta_data.get_settings_as_dict(), meta_data)
 
     def test_init(self):
         self.assertEqual(