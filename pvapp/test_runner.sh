b074c6a9970b76b108bb8c8ef498fc60169980e7
Matt Gibson <contact.matt.gibson@gmail.com>
2015-10-08 17:30:05 +1100

    REFACTOR: tidied up unused source

--- pvapp/test_runner.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-fswatch -e 'pyc' . |
-while read;
-    do python test/test_load_save.py;
-    echo "++++++++++++++++++++++"
-    echo "Finished running tests"
-    echo "++++++++++++++++++++++"
-done
-
